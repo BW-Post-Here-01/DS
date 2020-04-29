8f347e8e8ee8dd83319faddb2bdf501b73203b0d
ChrisHartig44 <59840488+ChrisHartig44@users.noreply.github.com>
2020-04-29 16:10:45 -0500

    Update reddit_app.py

--- reddit_app.py
+++ reddit_app.py
@@ -7,26 +7,38 @@ import pandas as pd
 from flask import Flask, request, render_template, redirect, jsonify
 # import _pickle as pickle
 from pickle import load
-loaded_model = load(open('reddit_model.pkl', 'rb'))
-application = app = Flask(__name__)
-@app.route("/")
-def home():
-    title = 'this is a title'
-    text = 'this is some text'
-    return redirect('/predict.json')
-@app.route("/predict.json", methods=["GET", "POST"])
+import sklearn
+import requests
+
+loaded_model = load(open('reddit_model_nc.pkl', 'rb'))
+app = Flask(__name__)
+@app.route("/test")
+def test():
+    # create the json object to send to the prediction function
+    title = 'This is the title of my test post'
+    text = "I've always wanted to post to Reddit, but I never know where!  Please tell me!"
+    content = {'title': title, 'text': text}
+    # send a post request to that prediction function
+    # note: this is hardcoded for localhost and will probably not work when deployed
+    url = 'http://localhost:5000/predict.json'
+    r = requests.post(url, json=content)
+    requests
+    # display the response
+    return r.text
+@app.route("/predict.json", methods=["POST"])
 def predict():
-  #> {'title': 'example title', 'text': 'Example reddit post text here'}
-  # concatenate title and text, passed in as one variable to the model
-  post = "this is a post"
-#   post = request.form["title"] + ' ' + request.form["text"]
-  # get predictions, store as a Pandas Series
-  #preds = pd.Series(loaded_model.predict_proba([post])[0])  # assign the subreddit classes to the index
-  #preds.index = loaded_model.classes_
-  # sort by values to get the top results
-  #preds = preds.sort_values(ascending=False)
-  # return the top 5 results as JSON
-  preds = pd.Series([.5, .4, .3, .2, .1])
-  preds.index = ['sub1', 'sub2', 'sub3', 'sub4', 'sub5']
-  return jsonify(subreddits=preds.index[:5].to_list(),
-                  probabilities=preds[:5].to_list())
+    #> {'title': 'example title', 'text': 'Example reddit post text here'}
+    request_data = request.get_json(force=True)
+    title = request_data['title']
+    text = request_data['text']
+    # concatenate title and text, passed in as one variable to the model
+    post = title + ' ' + text
+    # get predictions, store as a Pandas Series
+    preds = pd.Series(loaded_model.predict_proba([post])[0])
+    # assign the subreddit classes to the index
+    preds.index = loaded_model.classes_
+    # sort by values to get the top results
+    preds = preds.sort_values(ascending=False)
+    # return the top 5 results as JSON
+    return jsonify(subreddits=preds.index[:5].to_list(),
+                    probabilities=preds[:5].to_list())
